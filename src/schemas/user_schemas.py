6e411c2f1ffe37fab26ffb61bcdd7a87181d832f
ranzeet013 <ranzeetraut00@gmail.com>
2025-07-14 13:06:14 +0545

    fixed linting

--- src/schemas/user_schemas.py
+++ src/schemas/user_schemas.py
@@ -1,35 +1,3 @@
-# from pydantic import BaseModel, EmailStr
-
-# class UserCreate(BaseModel):
-#     name: str
-#     email: EmailStr
-#     password: str
-
-# class UserLogin(BaseModel):
-#     email: EmailStr
-#     password: str
-
-# class Token(BaseModel):
-#     access_token: str
-#     token_type: str
-
-# class UserOut(BaseModel):
-#     uuid: str
-#     name: str
-#     email: EmailStr
-#     is_verified: bool
-
-#     class Config:
-#         orm_mode = True
-
-# class ChangePassword(BaseModel):
-#     current_password: str
-#     new_password: str
-
-# class OTPVerify(BaseModel):
-#     email: EmailStr
-#     otp: str
-
 from pydantic import BaseModel, EmailStr
 from typing import TypeVar, Generic, Optional
 
